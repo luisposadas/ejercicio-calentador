d38850e6479b18a8d6c1ae42f31c685923f58353
Luis Posadas <luisposadasp96@outlook.es>
2017-08-23 18:37:22 -0500

    Se agrego  el max y el min

--- Calentador.java
+++ Calentador.java
@@ -1,19 +1,25 @@
 public class Calentador
 {
     private int temperatura;
-    public Calentador()
+    private int max;
+    private int min;
+    public Calentador(int minusr,int maxusr)
     {
+        max = maxusr;
+        min = minusr;
         temperatura = 15;
     }
     public void calentar()
     {
+        if((temperatura + 5) <= max){
         temperatura += 5;
-        
+    }
     }
     public void enfriar()
     {
+        if((temperatura - 5 )>= min){
         temperatura -= 5;
-        
+    }
     }
     public int muestraTemperatura()
     {
