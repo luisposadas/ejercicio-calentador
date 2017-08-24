6f885ac8d342a991d3a0e02b15a3b76c266b5182
Luis Posadas <luisposadasp96@outlook.es>
2017-08-23 19:14:28 -0500

    se agrego modificar incremento

--- Calentador.java
+++ Calentador.java
@@ -3,27 +3,35 @@ public class Calentador
     private int temperatura;
     private int max;
     private int min;
+    int incremento;
     public Calentador(int minusr,int maxusr)
     {
+        incremento = 5;
         max = maxusr;
         min = minusr;
         temperatura = 15;
     }
     public void calentar()
     {
-        if((temperatura + 5) <= max){
-        temperatura += 5;
+        if((temperatura + incremento) <= max){
+        temperatura += incremento;
     }
     }
     public void enfriar()
     {
-        if((temperatura - 5 )>= min){
-        temperatura -= 5;
+        if((temperatura - incremento )>= min){
+        temperatura -= incremento;
     }
     }
     public int muestraTemperatura()
     {
         return temperatura;
     }
-    
+    public void modificarIncremento(int incrementousr)
+    {
+        if(incrementousr >= 0){
+            incremento = incrementousr;
+        }
+        
+    }
 }
